cffc53f9088883b2562bd4f991c8dcc166438c43
sushmitaku <ksushmita1999@gmail.com>
2020-09-12 00:23:47 +0530

    Added:Doublet with all Possible combination Percentages

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -21,5 +21,36 @@ headPercentage=$(( $(( ${dictionary[H]}*100 ))/$limit ))
 tailPercentage=$(( $(( ${dictionary[T]}*100 ))/$limit ))
 echo "H= $headPercentage %"
 echo "T= $tailPercentage %"
- 
+dictionary[HH]=0
+dictionary[HT]=0
+dictionary[TH]=0
+dictionary[TT]=0
+for (( i=0; i<$limit; i++ ))
+do
+        coinFlip=$(( (RANDOM%4)+1 ))
+        if [ $coinFlip -eq 1 ]
+        then
+                dictionary[HH]=$(( ${dictionary[HH]}+1 ))
+        
+        elif [ $coinFlip -eq 2 ]
+        then
+                dictionary[HT]=$(( ${dictionary[HT]}+1 ))
+
+         elif [ $coinFlip -eq 3 ]
+         then
+                dictionary[TH]=$(( ${dictionary[TH]}+1 ))
+        else
+                dictionary[TT]=$(( ${dictionary[TT]}+1 ))
+        fi
+
+done
+HH=$(( $(( ${dictionary[HH]}*100 ))/$limit ))
+HT=$(( $(( ${dictionary[HT]}*100 ))/$limit ))
+TH=$(( $(( ${dictionary[TH]}*100 ))/$limit ))
+TT=$(( $(( ${dictionary[TT]}*100 ))/$limit ))
+echo "HH= $HH %"
+echo "HT= $HT %"
+echo "TH= $TH %"
+echo "TT= $TT %"
+
 
