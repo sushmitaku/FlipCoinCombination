4ff02b1c73babb83a6e3ff9ce4d3041fc7bd5fce
sushmitaku <ksushmita1999@gmail.com>
2020-09-11 23:17:53 +0530

    Added: Singlet Combination with Heads and Tails Percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,9 +1,25 @@
 Head=1
 Tail=0
-winner=$(( RANDOM %2 ))
-if [ $winner -eq $Head ]
-then
-	echo "Head is Winner"
-else
-	echo "Tail is Winner"
-fi
+#Declearing Dictionary
+declare -A dictionary
+dictionary[H]=0
+dictionary[T]=0
+read -p "Enter limit" limit
+for (( i=0; i<$limit; i++ ))
+do
+	winner=$(( RANDOM%2 ))
+	if [ $winner -eq $Head ]
+	then
+		dictionary[H]=$(( ${dictionary[H]}+1 ))
+	else
+		dictionary[T]=$(( ${dictionary[T]}+1 ))
+
+	fi
+done
+#echo ${dictionary[@]}
+headPercentage=$(( $(( ${dictionary[H]}*100 ))/$limit ))
+tailPercentage=$(( $(( ${dictionary[T]}*100 ))/$limit ))
+echo "H= $headPercentage %"
+echo "T= $tailPercentage %"
+ 
+
