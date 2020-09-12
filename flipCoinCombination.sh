2a134609ac308e35a45bfa6af76071640edc10e1
sushmitaku <ksushmita1999@gmail.com>
2020-09-12 14:27:28 +0530

    Added:Singlet Doublet Triplet sorted and Winning Combination Displayed

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -16,11 +16,30 @@ do
 
 	fi
 done
-#echo ${dictionary[@]}
 headPercentage=$(( $(( ${dictionary[H]}*100 ))/$limit ))
 tailPercentage=$(( $(( ${dictionary[T]}*100 ))/$limit ))
 echo "H= $headPercentage %"
 echo "T= $tailPercentage %"
+i=0
+array=()
+array[$i]=$headPercentage
+(( ++i ))
+array[$i]=$tailPercentage
+IFS=$'\n' sorted=($(sort -nr <<<"${array[*]}")); unset IFS
+maximumSinglet=${sorted[0]}
+u=0
+if [ $maximumSinglet -eq $headPercentage ]
+then
+        echo -n "H----> "
+        combine[$u]="H"
+        (( u++ ))
+else
+        echo -n "T----> "
+        combine[$u]="T"
+        (( u++ ))
+fi
+echo "$maximumSinglet %"
+
 dictionary[HH]=0
 dictionary[HT]=0
 dictionary[TH]=0
@@ -42,7 +61,6 @@ do
         else
                 dictionary[TT]=$(( ${dictionary[TT]}+1 ))
         fi
-
 done
 HH=$(( $(( ${dictionary[HH]}*100 ))/$limit ))
 HT=$(( $(( ${dictionary[HT]}*100 ))/$limit ))
@@ -52,7 +70,40 @@ echo "HH= $HH %"
 echo "HT= $HT %"
 echo "TH= $TH %"
 echo "TT= $TT %"
+s=0
+arrayDoublet=()
+arrayDoublet[$s]=$HH
+(( ++s ))
+arrayDoublet[$s]=$HT
+(( ++s))
+arrayDoublet[$s]=$TH
+(( ++s ))
+arrayDoublet[$s]=$TT
+IFS=$'\n' sortedDoublet=($(sort -nr <<<"${arrayDoublet[*]}")); unset IFS
+#echo ${sortedDoublet[@]}"
+maximumDoublet=${sortedDoublet[0]}
+if [ $maximumDoublet -eq $HH ]
+then
+        echo -n "HH----> "
+        combine[$u]="HH"
+        (( u++ ))
+elif [ $maximumDoublet -eq $HT ]
+then
+        echo -n "HT----> "
+        combine[$u]="HT"
+        (( u++ ))
+elif [ $maximumDoublet -eq $TH ]
+then
+        echo -n "TH----> "
+        combine[$u]="TH"
+        (( u++ ))
+else
+         echo -n "TT----> "
+        combine[$u]="TT"
+        (( u++ ))
 
+fi
+echo "$maximumDoublet %"
 dictionary[HHH]=0
 dictionary[HHT]=0
 dictionary[HTH]=0
@@ -97,8 +148,6 @@ do
         fi
 
 done
-
-
 HHH=$(( $(( ${dictionary[HHH]}*100 ))/$limit ))
 HHT=$(( $(( ${dictionary[HHT]}*100 ))/$limit ))
 HTH=$(( $(( ${dictionary[HTH]}*100 ))/$limit ))
@@ -116,5 +165,68 @@ echo "TTT= $TTT %"
 echo "HTT= $HTT %"
 echo "TTH= $TTH %"
 echo "THT= $THT %"
+k=0
+arrayTriplet=()
+arrayTriplet[$k]=$HHH
+(( ++k ))
+arrayTriplet[$k]=$HHT
+(( ++k))
+arrayTriplet[$k]=$HTH
+(( ++k ))
+arrayTriplet[$k]=$THH
+(( ++k ))
+arrayTriplet[$k]=$TTT
+(( ++k ))
+arrayTriplet[$k]=$HTT
+(( ++k))
+arrayTriplet[$k]=$TTH
+(( ++k ))
+arrayTriplet[$k]=$THT
+IFS=$'\n' sortedTriplet=($(sort -nr <<<"${arrayTriplet[*]}")); unset IFS
+#echo ${sortedTriplet[@]}"
+maximumTriplet=${sortedTriplet[0]}
+if [ $maximumTriplet -eq $HHH ]
+then
+        echo -n "HHH----> "
+        combine[$u]="HHH"
+        (( u++ ))
+elif [ $maximumTriplet -eq $HHT ]
+then
+        echo -n "HHT----> "
+        combine[$u]="HHT"
+        (( u++ ))
+elif [ $maximumTriplet -eq $HTH ]
+then
+        echo -n "HTH----> "
+        combine[$u]="HTH"
+        (( u++ ))
+elif [ $maximumTriplet -eq $THH ]
+then
+        echo -n "THH----> "
+        combine[$u]="THH"
+        (( u++ ))
+elif [ $maximumTriplet -eq $TTT ]
+then
+        echo -n "TTT----> "
+        combine[$u]="TTT"
+        (( u++ ))
+elif [ $maximumTriplet -eq $HTT ]
+then
+        echo -n "HTT----> "
+        combine[$u]="HTT"
+        (( u++ ))
+elif [ $maximumTriplet -eq $TTH ]
+then
+        echo -n "TTH----> "
+        combine[$u]="TTH"
+        (( u++ ))
+else
+         echo -n "THT----> "
+        combine[$u]="THT"
+fi
+echo "$maximumTriplet %"
+echo "Final Combination of Higest percentage $maximumSinglet % $maximumDoublet % $maximumTriplet %"
+echo "Higest percentage Combination is ${combine[@]} "
+
 
 
